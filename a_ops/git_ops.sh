b1b6d527feff59234a02d3d5ad0489a2a3e0fda1
LingXuan <251381922@qq.com>
2026-04-07 20:24:20 +0800

    mac_pro_tq_git

--- a_ops/git_ops.sh
+++ a_ops/git_ops.sh
@@ -1,4 +1,5 @@
-### Windows 微星主机
+# ----------- Windows 微星主机 --------------------
+
     # cd C:\Users\Murra\Desktop\code\kiro\kiro_agent
     # git pull origin main
 
@@ -14,5 +15,29 @@ git push -u origin windows_git:main
 
     # git init
     # git remote add origin git@github.com:lingxuan007/kiro_agent.git
-##################
+# --------------------------------------------
+
+
+
+# ----------- Mac Pro --------------------
+
+    # cd /Users/huanglingxuan/Documents/code/kiro/kiro_agent
+    # git pull origin main
+
+
+    # git branch
+    # git branch -m mac_pro_tq_git        # 重命名分支
+
+cd C:\Users\Murra\Desktop\code\kiro\kiro_agent
+git add --all .      
+git commit -m "mac_pro_tq_git"
+git push -u origin mac_pro_tq_git:main
+
+
+    # git init
+    # git remote add origin git@github.com:lingxuan007/kiro_agent.git
+# --------------------------------------------
+
+
+
 
